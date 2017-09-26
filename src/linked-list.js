fb8bc7a83b6acff517ed84cfffcb7781813a22f1
raikaat <raikaat@gmail.com>
2017-09-26 21:31:10 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -13,7 +13,7 @@ class LinkedList {
     this.head = node;
     this.tail = node;
     } else {
-    node.previous = this.tail;
+    node.prev = this.tail;
     this.tail.next = node;
     this.tail = node;		
     }
@@ -43,13 +43,13 @@ class LinkedList {
     var node = new Node(data);
     if (index === 0 && this.length>2) {
     node.next = this.head;
-    this.head.previous = node;
+    this.head.prev = node;
     this.head = node;
     }
     else if (index === 0 && this.length ===1) {
     node.next = this.tail;
     this.head = node;
-    this.tail.previous = this.head;
+    this.tail.prev = this.head;
     }
     else if ((index === 0) || ((index!= 1) && (index === this.length))) {
     return this.append(data);
@@ -60,8 +60,8 @@ class LinkedList {
     previousNode = previousNode.next;
     }
     var currentNode = previousNode.next;
-    currentNode.previous = node;
-    node.previous = previousNode;
+    currentNode.prev = node;
+    node.prev = previousNode;
     node.next = currentNode;
     previousNode.next = node;
     }
@@ -83,7 +83,7 @@ class LinkedList {
     return "Out of length";
     }
     var currentNode = this.head;
-	for (var i = 0; i < index-1; i++) {
+    for (var i = 0; i < index-1; i++) {
     currentNode = currentNode.next;
     }
     if ((currentNode === this.head) && (currentNode === this.tail)) {
@@ -92,15 +92,15 @@ class LinkedList {
     }
     else if (currentNode === this.head) {
     this.tail = currentNode.next;
-    currentNode.previous = null;
+    currentNode.prev = null;
     }
     else if (currentNode === this.tail) {
-    this.tail = currentNode.previous;
+    this.tail = currentNode.prev;
     currentNode.next = null;
     }
     else {
-    currentNode.previous.next =currentNode.next;
-    currentNode.next.previous = currentNode.previous; 
+    currentNode.prev.next = currentNode.next;
+    currentNode.next.prev = currentNode.prev; 
     }
     this.length --;
     }
